d80552329203a6a080ed1f04e05e7ba8d3ff785c
TammamWafai <47179662+TammamWafai@users.noreply.github.com>
2022-07-17 20:27:22 -0500

    Messages section, remove btn added

--- js/index.js
+++ js/index.js
@@ -17,3 +17,49 @@ for (let i = 0; i < skills.length; i++) {
 }
 
 
+//Global variables
+let messageSection = document.querySelector('#messages');
+let messageList = messageSection.querySelector('ul');
+let messageForm = document.querySelector('[name="leave_message"]')
+
+//Submit handling
+messageForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    let name = event.target.name;
+    let email = event.target.email;
+    let message = event.target.message;
+    let newMessage = document.createElement('li');
+
+    newMessage.innerHTML = "<a href='mailto:" + email.value + "'>" + name.value + "</a> wrote: <span>" + message.value + "</span>   ";
+    messageList.appendChild(newMessage);
+    let removeButton = document.createElement('button');
+    removeButton.innerText = "remove";
+    removeButton.setAttribute("type", "button");
+    removeButton.addEventListener('click', (event) => {
+        let entry = removeButton.parentNode;
+        entry.remove();
+        messagesSectionShowHide();
+    })
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+
+    console.log(name.value, email.value, message.value)
+    messagesSectionShowHide();
+    messageForm.reset();
+});
+
+//Check if Message section is empty on page load
+window.addEventListener("load", function () {
+    messagesSectionShowHide();
+});
+
+// Show/hide Messages section
+function messagesSectionShowHide() {
+    if (messageList.childElementCount != 0) {
+        messageSection.style.display = "block";
+    } else {
+        messageSection.style.display = "none";
+    }
+}
+
